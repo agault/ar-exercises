46cf6ede755243d465e9616e62321ccebb6b9355
Asia Gault <asialevesquegault@gmail.com>
2017-08-29 16:09:50 -0400

    part 6 finished

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -5,6 +5,24 @@ require_relative './exercise_3'
 require_relative './exercise_4'
 require_relative './exercise_5'
 
+@store1.employee.create(first_name: "Khurram", last_name: "Vii", hourly_rate: 60)
+@store1.employee.create(first_name: "apples", last_name: "Vini", hourly_rate: 60)
+@store1.employee.create(first_name: "asia", last_name: "Virani", hourly_rate: 90)
+@store2.employee.create(first_name: "journey", last_name: "Vani", hourly_rate: 70)
+@store2.employee.create(first_name: "tyler", last_name: "Vrani", hourly_rate: 90)
+@store2.employee.create(first_name: "Kim", last_name: "Virni", hourly_rate: 90)
+@store2.employee.create(first_name: "Khm", last_name: "Viran", hourly_rate: 60)
+
+
+
+
+
+
+
+
+
+
+
 puts "Exercise 6"
 puts "----------"
 
