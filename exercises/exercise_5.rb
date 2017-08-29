5e080ecc005a07adb312610112b88dfb25d39cdd
Asia Gault <asialevesquegault@gmail.com>
2017-08-29 16:03:03 -0400

    finished 5

--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -4,7 +4,14 @@ require_relative './exercise_2'
 require_relative './exercise_3'
 require_relative './exercise_4'
 
+@sumz = Store.sum(:annual_revenue)
+@store_num = Store.count
+
+@big_spenders = Store.where("annual_revenue > 1000000").count
+
 puts "Exercise 5"
-puts "----------"
+puts @sumz.as_json
+puts @sumz.as_json/@store_num.as_json
+puts @big_spenders.as_json
 
 # Your code goes here ...
