024176cec5826ce26e0c6808cf9de2c6e4d7defa
Asia Gault <asialevesquegault@gmail.com>
2017-08-29 15:55:07 -0400

    finished 4

--- exercises/exercise_4.rb
+++ exercises/exercise_4.rb
@@ -7,7 +7,16 @@ e = Store.create(:name => "Whistler", :annual_revenue => 1260000, :mens_apparel 
 f = Store.create(:name => "Yaletown ", :annual_revenue => 190000, :mens_apparel =>  true, :womens_apparel =>  true)
 
 @mens_stores = Store.where(mens_apparel: true)
+
+
+@mens_stores.each do |store|
+  puts store.name
+  puts store.annual_revenue
+end
+
+@womens_less_mill = Store.where(womens_apparel: true).where("annual_revenue < 1000000")
+
 puts "Exercise 4"
 puts @mens_stores.as_json
-
+puts @womens_less_mill.as_json
 # Your code goes here ...
