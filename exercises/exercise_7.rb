a86fef3099021c7bc328c1f15b863805c30b63ad
Asia Gault <asialevesquegault@gmail.com>
2017-08-29 16:53:25 -0400

    LAST

--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -6,7 +6,17 @@ require_relative './exercise_4'
 require_relative './exercise_5'
 require_relative './exercise_6'
 
+
+
 puts "Exercise 7"
-puts "----------"
+puts "Enter a store:"
+print "==>"
+@storex = gets.chomp
+@storex
+@new_store = Store.create(:name => @storex)
+@new_store.save
+@new_store.errors.to_a.each do |error|
+  puts "ERROR: #{error}"
+end
 
 # Your code goes here ...
--- lib/store.rb
+++ lib/store.rb
@@ -1,3 +1,13 @@
 class Store < ActiveRecord::Base
   has_many :employee
+  validates :name, length: { minimum: 3 }
+  validates :annual_revenue, numericality: { only_integer: true, greater_than: 0 }
+  validate :store_has_mens_or_womens_apparel, on: :create
+
+
+def store_has_mens_or_womens_apparel
+   if !(mens_apparel == true || womens_apparel == true)
+      errors[:base] << 'Store needs to carry men\'s or women\'s apparel'
+    end
+  end
 end
